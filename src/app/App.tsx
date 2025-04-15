cf25288eda31c8fcb6ae6dedbace99b79754fd56
Caner Iskenderoglu <caner.iskenderoglu@gmx.de>
2025-04-15 14:56:13 +0200

    vitest and testing library

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
+import reactLogo from '@/assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 
@@ -16,9 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1 className="bg-amber-50 text-3xl font-bold underline">
-        Hello world! Vite + React
-      </h1>
+      <h1 className="bg-amber-50 text-3xl font-bold underline">Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
