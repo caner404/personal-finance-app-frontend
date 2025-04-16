cdd11bf5fb680f65ed545a89e54a82b1a16156a6
Caner Iskenderoglu <caner.iskenderoglu@gmx.de>
2025-04-16 09:48:47 +0200

    feat: add Button component with variants and corresponding stories

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import reactLogo from '@/assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
+import { Button } from '@/components/Button';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -9,6 +10,8 @@ function App() {
   return (
     <>
       <div>
+        <Button variant="outline">Outline</Button>
+
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
